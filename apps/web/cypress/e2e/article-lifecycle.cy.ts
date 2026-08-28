ca6717ecff258dbf64dba96b78b86ac3cd543f43
malindugurunada1BT <malindu.gurunada@1billiontech.com>
2026-08-28 15:08:12 +0530

    fix CI error

--- apps/web/cypress/e2e/article-lifecycle.cy.ts
+++ apps/web/cypress/e2e/article-lifecycle.cy.ts
@@ -405,6 +405,32 @@ describe('Article lifecycle', () => {
 
         expect(interception.response?.statusCode).to.eq(200);
         const responseBody = interception.response?.body;
+        if (responseBody?.data?.id === undefined) {
+          throw new Error(
+            `Reviewer article detail interception diagnostic:\n${JSON.stringify(
+              {
+                request: {
+                  url: interception.request.url,
+                  method: interception.request.method,
+                  headers: {
+                    'x-test-user-id':
+                      interception.request.headers['x-test-user-id'],
+                    'x-test-user-email':
+                      interception.request.headers['x-test-user-email'],
+                    'x-test-user-role':
+                      interception.request.headers['x-test-user-role'],
+                  },
+                },
+                response: {
+                  statusCode: interception.response?.statusCode,
+                  body: interception.response?.body,
+                },
+              },
+              null,
+              2
+            )}`
+          );
+        }
         expect(responseBody.success).to.eq(true);
         expect(responseBody.data.id).to.eq(articleId);
         expect(responseBody.data.title).to.eq(articleTitle);
